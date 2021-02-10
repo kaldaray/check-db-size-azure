6d90613b34c214b6fd8b5b7a3d9b4e1de4ececa0
Nikolay Fofilov <46383482+kaldaray@users.noreply.github.com>
2021-02-10 18:07:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 ### Подключение к базе данных
 
 class DbConnect:
-    server = 'tzzrt0aqpb.database.secure.windows.net'
-    database = 'FicoDevDB'
-    username = 'fico_monitoring'
-    password = '42XeLg5ZbmUEmq'
-    driver= '{ODBC Driver 13 for SQL Server}'
+    server = ''
+    database = ''
+    username = ''
+    password = ''
+    driver= ''
